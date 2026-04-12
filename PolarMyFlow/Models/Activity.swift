016acb2b224c3327418b1d481d1d36063a8ccf41
Esa Sepänniemi <esa.sepanniemi@gmail.com>
2026-04-12 20:13:06 +0300

    feat: add Activity and SyncState SwiftData models

--- PolarMyFlow/Models/Activity.swift
+++ PolarMyFlow/Models/Activity.swift
@@ -1,1 +1,51 @@
-// TODO: implement
+import Foundation
+import SwiftData
+
+@Model
+final class Activity {
+    @Attribute(.unique) var id: String
+    var startTime: Date
+    var duration: TimeInterval      // seconds
+    var distance: Double            // metres
+    var sportRawValue: String       // Polar sport string e.g. "CROSS_COUNTRY_SKIING"
+    var avgSpeed: Double            // m/s
+    var avgPace: Double             // s/m (seconds per metre; display as min/km)
+    var avgHeartRate: Int?
+    var maxHeartRate: Int?
+    var ascent: Double?             // metres gained
+    var descent: Double?            // metres lost
+    var calories: Int?
+    var hasRoute: Bool
+
+    var sportType: SportType { SportType(polarString: sportRawValue) }
+
+    init(
+        id: String,
+        startTime: Date,
+        duration: TimeInterval,
+        distance: Double,
+        sportRawValue: String,
+        avgSpeed: Double,
+        avgPace: Double,
+        avgHeartRate: Int? = nil,
+        maxHeartRate: Int? = nil,
+        ascent: Double? = nil,
+        descent: Double? = nil,
+        calories: Int? = nil,
+        hasRoute: Bool = false
+    ) {
+        self.id = id
+        self.startTime = startTime
+        self.duration = duration
+        self.distance = distance
+        self.sportRawValue = sportRawValue
+        self.avgSpeed = avgSpeed
+        self.avgPace = avgPace
+        self.avgHeartRate = avgHeartRate
+        self.maxHeartRate = maxHeartRate
+        self.ascent = ascent
+        self.descent = descent
+        self.calories = calories
+        self.hasRoute = hasRoute
+    }
+}
--- PolarMyFlow/Models/SyncState.swift
+++ PolarMyFlow/Models/SyncState.swift
@@ -1,1 +1,17 @@
-// TODO: implement
+import Foundation
+import SwiftData
+
+@Model
+final class SyncState {
+    var userID: String
+    var lastSyncedAt: Date?
+    var accessLinkRegistered: Bool
+    var historicalImportComplete: Bool
+
+    init(userID: String) {
+        self.userID = userID
+        self.lastSyncedAt = nil
+        self.accessLinkRegistered = false
+        self.historicalImportComplete = false
+    }
+}
