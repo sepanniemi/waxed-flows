5975f8a73d07bc9bab5c6a614af205e1ad422edd
Esa Sepänniemi <esa.sepanniemi@gmail.com>
2026-04-12 17:26:57 +0300

    feat: add SportType enum with Polar string mapping

--- PolarMyFlow/Models/SportType.swift
+++ PolarMyFlow/Models/SportType.swift
@@ -1,1 +1,56 @@
-// TODO: implement
+enum SportType: String, Codable, CaseIterable, Equatable {
+    case xcSkiing = "CROSS_COUNTRY_SKIING"
+    case running = "RUNNING"
+    case cycling = "CYCLING"
+    case swimming = "SWIMMING"
+    case hiking = "HIKING"
+    case strength = "STRENGTH_TRAINING"
+    case rowing = "ROWING"
+    case other = "OTHER"
+
+    init(polarString: String) {
+        self = SportType(rawValue: polarString) ?? .other
+    }
+
+    var displayName: String {
+        switch self {
+        case .xcSkiing:
+            return "XC Skiing"
+        case .running:
+            return "Running"
+        case .cycling:
+            return "Cycling"
+        case .swimming:
+            return "Swimming"
+        case .hiking:
+            return "Hiking"
+        case .strength:
+            return "Strength"
+        case .rowing:
+            return "Rowing"
+        case .other:
+            return "Other"
+        }
+    }
+
+    var symbolName: String {
+        switch self {
+        case .xcSkiing:
+            return "figure.skiing.crosscountry"
+        case .running:
+            return "figure.run"
+        case .cycling:
+            return "figure.outdoor.cycle"
+        case .swimming:
+            return "figure.pool.swim"
+        case .hiking:
+            return "figure.hiking"
+        case .strength:
+            return "dumbbell"
+        case .rowing:
+            return "figure.rowing"
+        case .other:
+            return "figure.mixed.cardio"
+        }
+    }
+}
--- PolarMyFlowTests/SportTypeTests.swift
+++ PolarMyFlowTests/SportTypeTests.swift
@@ -1,1 +1,40 @@
-// TODO: implement
+import XCTest
+@testable import PolarMyFlow
+
+final class SportTypeTests: XCTestCase {
+
+    func test_knownSport_xcSkiing() {
+        XCTAssertEqual(SportType(polarString: "CROSS_COUNTRY_SKIING"), .xcSkiing)
+    }
+
+    func test_knownSport_running() {
+        XCTAssertEqual(SportType(polarString: "RUNNING"), .running)
+    }
+
+    func test_knownSport_cycling() {
+        XCTAssertEqual(SportType(polarString: "CYCLING"), .cycling)
+    }
+
+    func test_knownSport_swimming() {
+        XCTAssertEqual(SportType(polarString: "SWIMMING"), .swimming)
+    }
+
+    func test_unknownSport_fallsBackToOther() {
+        XCTAssertEqual(SportType(polarString: "UNDERWATER_HOCKEY"), .other)
+        XCTAssertEqual(SportType(polarString: ""), .other)
+    }
+
+    func test_displayName_xcSkiing() {
+        XCTAssertEqual(SportType.xcSkiing.displayName, "XC Skiing")
+    }
+
+    func test_symbolName_xcSkiing() {
+        XCTAssertEqual(SportType.xcSkiing.symbolName, "figure.skiing.crosscountry")
+    }
+
+    func test_symbolName_other_isValid() {
+        for sport in SportType.allCases {
+            XCTAssertFalse(sport.symbolName.isEmpty, "\(sport) has empty symbolName")
+        }
+    }
+}
