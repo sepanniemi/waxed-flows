6769c08c3c4009fbf826bb4be53bb3febb0ae2f2
Esa Sepänniemi <esa.sepanniemi@gmail.com>
2026-06-02 18:25:26 +0300

    chore: add MML open-data API key to BuildConfig

--- PolarMyFlow/BuildConfig.swift
+++ PolarMyFlow/BuildConfig.swift
@@ -6,4 +6,6 @@ enum BuildConfig {
     static let clientID     = "c045142a-470a-4d0c-8f44-b8aa1200e975"
     static let clientSecret = "59ccbd6e-aaae-4881-b121-a3b7774ff03b"
     #endif
+
+    static let mmlApiKey    = "5226aad5-8658-4e13-add5-efb53ea36990"
 }
